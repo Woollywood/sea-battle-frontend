f6234f5ceb092463ce8f12c2e349fedbd604d067
a.cherkashin <a.cherkashin@notamedia.com.ru>
2025-08-18 17:17:50 +0300

    feat: shadcn, vueuse, tailwind

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <Button>click me</Button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,24 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['~/assets/styles/tailwind.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
-})
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image', 'shadcn-nuxt', '@vueuse/nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: '~/components/ui',
+  },
+});
